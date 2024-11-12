218dedb5f96446d135cca59d45b34ea31bd20ea9
sadafshahabsr12 <sadafshahabsr12@gmail.com>
2024-11-12 09:01:38 +0500

    done

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-// postcss.config.js (ES Module)
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
