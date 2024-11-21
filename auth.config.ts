919f57d60477fb1a85fa77bfd47cdf7e1d3df7f7
sadafshahabsr12 <sadafshahabsr12@gmail.com>
2024-11-21 12:50:58 +0500

    fixing 404 issue

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,6 @@
 import type { NextAuthConfig } from 'next-auth';
- 
-export const authConfig = {
+
+export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/login',
   },
@@ -8,14 +8,17 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+
+      if (isOnDashboard && !isLoggedIn) {
+        return Response.redirect(new URL('/login', nextUrl));
+      }
+
+      if (isLoggedIn && nextUrl.pathname === '/login') {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
+
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+  providers: [], 
+};
