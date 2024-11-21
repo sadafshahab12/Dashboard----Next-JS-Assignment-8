afb0780fa34690dedfe27c90d47c8c69bb80f3a2
sadafshahabsr12 <sadafshahabsr12@gmail.com>
2024-11-21 12:23:34 +0500

    bug fix

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -21,7 +21,10 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = fetchInvoicesPages(query);
+
+  // Wait for the totalPages value to resolve
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { generatePagination } from "@/app/lib/utils";
 import { usePathname, useSearchParams } from "next/navigation";
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
+export default function Pagination({ totalPages }: { totalPages: number  }) {
   // NOTE: Uncomment this code in Chapter 11
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -23,7 +23,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
-
       <div className="inline-flex">
         <PaginationArrow
           direction="left"
